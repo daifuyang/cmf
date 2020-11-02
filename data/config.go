1460b595de0eee8457d304fc05d2df1b921a9fe9
1140444693@qq.com <1140444693@qq.com>
2020-11-02 15:09:00 +0800

    修改数据库配置，增加redis支持！

--- data/config.go
+++ data/config.go
@@ -5,35 +5,57 @@
  */
 package data
 
-//App 配置文件app对象 定义了系统的基础信息
-type AppStruct struct {
+// App 配置文件app对象 定义了系统的基础信息
+type App struct {
 	Port     string
 	AppName  string
 	AppDebug bool
 	Domain   string
 }
 
-//Database 配置文件数据库对象 定义了数据库的基础信息
-type DatabaseStruct struct {
+// Database 配置文件数据库对象 定义了数据库的基础信息
+type Database struct {
+	Default  string `json:"default"`
 	Type     string `json:"type"`
-	Host     string `json:"hostname"`
+	Host     string `json:"host"`
 	Name     string `json:"database"`
 	User     string `json:"username"`
 	Pwd      string `json:"password"`
-	Port     string `json:"hostport"`
+	Port     string `json:"port"`
 	Charset  string `json:"charset"`
 	Prefix   string `json:"prefix"`
 	AuthCode string `json:"authcode"`
 }
 
+// Redis 配置文件对象
+type Redis struct {
+	Type     string `json:"type"`
+	Host     string `json:"host"`
+	Pwd      string `json:"password"`
+	Port     string `json:"port"`
+	Database int    `json:"database"`
+}
+
 //ConfigDefault 定义了配置文件初始结构
-type ConfigDefaultStruct struct {
-	App      AppStruct
+type ConfigDefault struct {
+	App      App
 	Template TemplateMapStruct `json:"template"`
-	Database DatabaseStruct
+	Database Database
+	Redis    Redis
 	Token    string
 }
 
 //ConfigData 定义一个空结构体
-type ConfigDataStruct struct {
+type ConfigData struct {
+}
+
+type TempDataBase struct {
+	Default string   `json:"default"`
+	Mysql   Database `json:"mysql"`
+	Redis   Redis `json:"redis"`
+}
+
+// 定义读取配置缓存文件
+type TempConfig struct {
+	Database TempDataBase
 }
